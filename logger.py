449e8b96f4477e8ef7d14c3ced85a878efa2d835
HorngKimSomnang <kimsomnang124@gmail.com>
2026-04-01 13:55:27 +0700

    feat: implement modulo() and update log format {#{8}}

--- logger.py
+++ logger.py
@@ -1,8 +1,6 @@
 import datetime
 
 def log_action(action_name, result):
-    # TODO: {#{13}} Initial log format
-    # This will be modified by multiple members to cause conflicts
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open("calc_history.log", "a") as f:
-        f.write(f"[{timestamp}] {action_name}: {result}\n")
+        f.write(f"[{timestamp}] ✅ {action_name}: {result}\n")
