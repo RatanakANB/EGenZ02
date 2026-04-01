77c4ca79f92ae3014850d58b94b6010e8f5dd722
drintz36 <sorintdyz36@gmail.com>
2026-04-01 17:18:36 +0700

    5

--- logger.py
+++ logger.py
@@ -3,4 +3,8 @@ import datetime
 def log_action(action_name, result):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open("calc_history.log", "a") as f:
+<<<<<<< HEAD
         f.write(f"[{timestamp}] ACTION={action_name}RESULT={result}\n")
+=======
+        f.write(f"[{timestamp}] {action_name}: 😍{result}\n")
+>>>>>>> ddb4d7f (fix: logger and cal2)
