5c6984674146d8d6acf10f9a6b866503109495f5
AnB School 015 <anbschool015@AnBs-Mac-mini-2.local>
2026-04-01 18:45:57 +0700

    hotfix: z

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ def multiply(a, b):
 
 def divide(a, b):
     if (b==0):
-        raise ValueError ("Cannot divide by Zero")
+        raise ValueError ("Cannot divide by zero")
     result = a/b
     log_action("divide", result)
     return result
