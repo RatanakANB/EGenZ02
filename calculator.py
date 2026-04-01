e128af27fcf6b0f4a39d4316975fea74827d6745
AnB School 015 <anbschool015@AnBs-Mac-mini-2.local>
2026-04-01 18:27:55 +0700

    resolve conflict1

--- calculator.py
+++ calculator.py
@@ -50,9 +50,7 @@ def factorial(n):
     result = math.factorial(int(n))
     log_action("factorial", result)
     return result
-
-   
-
+    
 
 def absolute(a):
     # TODO: {#{11}} Member 06: Implement absolute value
